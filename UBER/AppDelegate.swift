3dd8d9f1b18d996b2255f5f6e19f8186fa3f8242
Ayubov Bakha <ayubov.b@gmail.com>
2017-10-21 04:37:15 +0600

    FirstCommit PodAdded

--- UBER/AppDelegate.swift
+++ UBER/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import IQKeyboardManager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        IQKeyboardManager.shared().isEnabled = true
         
         UINavigationBar.appearance().backgroundColor = Colors.getDarkBlue()
         UINavigationBar.appearance().tintColor = UIColor.white
