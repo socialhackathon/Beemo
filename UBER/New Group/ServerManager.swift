c0aea0c97df85e516342a9de85d960f1bd95c43d
Ayubov Bakha <ayubov.b@gmail.com>
2017-10-21 18:05:31 +0600

    updateservermanager

--- UBER/New Group/ServerManager.swift
+++ UBER/New Group/ServerManager.swift
@@ -27,12 +27,20 @@ class ServerManager: HTTPRequestManager  {
     }
     
     func login(login: String, password: String, completion: @escaping (Int)-> Void,error: @escaping (String)-> Void) {
+        var token = ""
         self.post(api: "login", parameters: ["username": login, "password": password], completion: { (json) in
-            let token = json["token"].stringValue
-            UserDefaults.standard.set(token, forKey: "token")
-            let user_id = json["user_id"].intValue
-            //print(json)
-            completion(user_id)
+            token = json["token"].stringValue
+            let parameter = ["key": token]
+            self.post(api: "user_id", parameters: parameter, completion: { json in
+                let user_id = json["user_id"].intValue
+                UserDefaults.standard.set(user_id, forKey: "user_id")
+                completion(user_id)
+            }, error: error)
         }, error: error)
+        if token != "" {
+            UserDefaults.standard.set(token, forKey: "token")
+        }
     }
+    
+    
 }
