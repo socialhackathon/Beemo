56bc808af724916bb783e9cf9105a890e8426806
Ayubov Bakha <ayubov.b@gmail.com>
2017-10-21 20:16:36 +0600

    Update

--- UBER/Profile/ProfileViewController.swift
+++ UBER/Profile/ProfileViewController.swift
@@ -16,20 +16,7 @@ class ProfileViewController: UIViewController {
         self.title = "Профиль"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func logoutButton(_ sender: Any) {
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
